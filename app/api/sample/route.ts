ecf707bb97a6c443b4b192d2c15ed45c802edc04
Madhan <madhanreddy400@gmail.com>
2025-04-23 19:22:39 +0530

    added mongo/prisma

---
+++ app/api/sample/route.ts
@@ -0,0 +1,20 @@
+//TESTING the working of prisma
+
+import { PrismaClient } from "@/lib/generated/prisma";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+    const user = await prisma.user.create({
+        data: {
+            email: "manu@gmail.com",
+            name: "Manu", 
+            hashed_password: "123456"
+        },
+    });
+    console.log(user);
+    // This is a sample API route that returns a JSON response
+    return new Response(JSON.stringify({ message: "Hello from the sample API route!" }), {
+        headers: { "Content-Type": "application/json" },
+    });
+}
